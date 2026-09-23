24190e83be7189aea1c7f05334c923e834417124
Dmitry Kurochkin <kudima03@gmail.com>
2026-09-23 09:21:27 +0000

    feat: add joined and empty-result where query samples (#7)
    Add 9 Where samples ported from the JoinedFilter and EmptyResult tests
    of Pure.RelationalSchema.Storage.PureQL.Projection: filters over joined
    columns and filters that match nothing, alone and followed by GROUP BY,
    HAVING and pagination.

---
+++ src/Tests/PureQL.CSharp.Model.Samples.Tests/Queries/Where/WhereConjunctionAcrossBothTablesQueryTests.cs
@@ -0,0 +1,93 @@
+using PureQL.CSharp.Model.Samples.Queries.Where;
+
+namespace PureQL.CSharp.Model.Samples.Tests.Queries.Where;
+
+public sealed record WhereConjunctionAcrossBothTablesQueryTests
+{
+    [Fact]
+    public void ValueSerializesToExpectedJson()
+    {
+        Assert.Equal(
+            new ExpectedJson(
+                /*lang=json,strict*/
+                """
+                {
+                  "from": {
+                    "entity": "schema_with_foreign_keys.orders"
+                  },
+                  "select": [
+                    {
+                      "entity": "schema_with_foreign_keys.orders",
+                      "field": "order_id",
+                      "type": {
+                        "name": "uuid"
+                      }
+                    }
+                  ],
+                  "where": {
+                    "operator": "eachAnd",
+                    "conditions": [
+                      {
+                        "operator": "eachGreaterThanOrEqual",
+                        "left": {
+                          "entity": "schema_with_foreign_keys.users",
+                          "field": "user_age",
+                          "type": {
+                            "name": "number"
+                          }
+                        },
+                        "right": {
+                          "type": {
+                            "name": "number"
+                          },
+                          "value": 30
+                        }
+                      },
+                      {
+                        "operator": "eachGreaterThan",
+                        "left": {
+                          "entity": "schema_with_foreign_keys.orders",
+                          "field": "order_total",
+                          "type": {
+                            "name": "number"
+                          }
+                        },
+                        "right": {
+                          "type": {
+                            "name": "number"
+                          },
+                          "value": 100
+                        }
+                      }
+                    ]
+                  },
+                  "joins": [
+                    {
+                      "type": "inner",
+                      "entity": "schema_with_foreign_keys.users",
+                      "on": {
+                        "operator": "eachEqual",
+                        "left": {
+                          "entity": "schema_with_foreign_keys.orders",
+                          "field": "order_user_id",
+                          "type": {
+                            "name": "uuid"
+                          }
+                        },
+                        "right": {
+                          "entity": "schema_with_foreign_keys.users",
+                          "field": "user_id",
+                          "type": {
+                            "name": "uuid"
+                          }
+                        }
+                      }
+                    }
+                  ]
+                }
+                """
+            ).TextValue,
+            new QueryJson(new WhereConjunctionAcrossBothTablesQuery().Value).TextValue
+        );
+    }
+}
---
+++ src/Tests/PureQL.CSharp.Model.Samples.Tests/Queries/Where/WhereNegationOnJoinedColumnQueryTests.cs
@@ -0,0 +1,75 @@
+using PureQL.CSharp.Model.Samples.Queries.Where;
+
+namespace PureQL.CSharp.Model.Samples.Tests.Queries.Where;
+
+public sealed record WhereNegationOnJoinedColumnQueryTests
+{
+    [Fact]
+    public void ValueSerializesToExpectedJson()
+    {
+        Assert.Equal(
+            new ExpectedJson(
+                /*lang=json,strict*/
+                """
+                {
+                  "from": {
+                    "entity": "schema_with_foreign_keys.orders"
+                  },
+                  "select": [
+                    {
+                      "entity": "schema_with_foreign_keys.orders",
+                      "field": "order_id",
+                      "type": {
+                        "name": "uuid"
+                      }
+                    }
+                  ],
+                  "where": {
+                    "operator": "eachNot",
+                    "condition": {
+                      "operator": "eachEqual",
+                      "left": {
+                        "entity": "schema_with_foreign_keys.users",
+                        "field": "user_name",
+                        "type": {
+                          "name": "string"
+                        }
+                      },
+                      "right": {
+                        "type": {
+                          "name": "string"
+                        },
+                        "value": "Ann"
+                      }
+                    }
+                  },
+                  "joins": [
+                    {
+                      "type": "inner",
+                      "entity": "schema_with_foreign_keys.users",
+                      "on": {
+                        "operator": "eachEqual",
+                        "left": {
+                          "entity": "schema_with_foreign_keys.orders",
+                          "field": "order_user_id",
+                          "type": {
+                            "name": "uuid"
+                          }
+                        },
+                        "right": {
+                          "entity": "schema_with_foreign_keys.users",
+                          "field": "user_id",
+                          "type": {
+                            "name": "uuid"
+                          }
+                        }
+                      }
+                    }
+                  ]
+                }
+                """
+            ).TextValue,
+            new QueryJson(new WhereNegationOnJoinedColumnQuery().Value).TextValue
+        );
+    }
+}
